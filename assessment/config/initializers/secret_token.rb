96b0d7e7059d92c6953baca7afe5b6b7f07d8d65
hannah nordgren <hannah.nordgren@flatironschool.com>
2014-03-11 18:02:11 -0400

    finished rails assessment

---
+++ assessment/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuickBlog::Application.config.secret_token = 'cec6ad5851f92432c8a506327a45a9814ebebfa03f9e271476605b8abd74f0f4be98a82e8d6aa460c54a67e735ccdfc0188d2711fcce54ee85985b20caf51776'
